0f6df09dd1e49f085589df084ef3a832cec5394a
Yasser Bouyanfif <yasser.bouyanfif@gmail.com>
2025-10-25 00:09:36 +0200

    if

--- src/App.js
+++ src/App.js
@@ -154,10 +154,10 @@ function App() {
                 Électricien généraliste pour particuliers & professionnels
               </span>
               <h1>
-                <span className="hero-location">
+                <span className="hero-highlight">
                   Électricien dans la Haute-Garonne
                 </span>{" "}
-                <span className="hero-highlight">(Auribail – Toulouse)</span>
+                <span className="hero-location">(Auribail – Toulouse)</span>
               </h1>
               <p className="hero-description">
                 Hark-Elec 31, dirigée par Patrice Fonteneau, prend en charge
